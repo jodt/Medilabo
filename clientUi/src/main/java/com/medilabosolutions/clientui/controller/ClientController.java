176d207167aaf5f4a8950dc3f9a75c097739c790
joel Dumortier <joel.dumortier@gmail.com>
2024-04-26 15:50:26 +0200

    create clientController

---
+++ clientUi/src/main/java/com/medilabosolutions/clientui/controller/ClientController.java
@@ -0,0 +1,28 @@
+package com.medilabosolutions.clientui.controller;
+
+import com.medilabosolutions.clientui.beans.PatientBean;
+import com.medilabosolutions.clientui.proxies.PatientProxy;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ClientController {
+
+    private final PatientProxy patientProxy;
+
+    public ClientController(PatientProxy patientProxy) {
+        this.patientProxy = patientProxy;
+    }
+
+    @GetMapping("/")
+    public String allPatientPage(Model model) {
+        List<PatientBean> patients = this.patientProxy.getAllPatients();
+        model.addAttribute("patients", patients);
+        return ("/allPatientsPage");
+    }
+
+
+}
