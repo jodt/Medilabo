fdc00ce61292cf89bb722a02c0ebc3615d0a5008
joel Dumortier <joel.dumortier@gmail.com>
2024-05-09 09:31:52 +0200

    ignore fields

--- patient/src/main/java/com/medilabosolutions/patient/mapper/PatientMapper.java
+++ patient/src/main/java/com/medilabosolutions/patient/mapper/PatientMapper.java
@@ -7,12 +7,13 @@ import com.medilabosolutions.patient.model.Patient;
 import fr.xebia.extras.selma.EnumMapper;
 import fr.xebia.extras.selma.Mapper;
 
-@Mapper(withIgnoreFields = {"com.medilabosolutions.patient.model.Address.id"})
+@Mapper(withIgnoreFields="com.medilabosolutions.patient.model.Address.id")
 public interface PatientMapper {
 
     @EnumMapper(from = GenderEnum.class, to = GenderEnumDto.class)
     PatientDto asPatientDto(Patient patient);
 
     @EnumMapper(from = GenderEnumDto.class, to = GenderEnum.class)
+    //@Maps(withIgnoreFields={"com.medilabosolutions.patient.model.Patient.id","com.medilabosolutions.patient.dto.PatientDto.id"})
     Patient asPatient(PatientDto patientDto);
 }
