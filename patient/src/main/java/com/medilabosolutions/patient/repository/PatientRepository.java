b8351bf96f285f303862b854350b0b66391baac4
joel Dumortier <joel.dumortier@gmail.com>
2024-05-09 09:33:21 +0200

    remove unecessary import

--- patient/src/main/java/com/medilabosolutions/patient/repository/PatientRepository.java
+++ patient/src/main/java/com/medilabosolutions/patient/repository/PatientRepository.java
@@ -4,7 +4,6 @@ import com.medilabosolutions.patient.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.Optional;
 
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
