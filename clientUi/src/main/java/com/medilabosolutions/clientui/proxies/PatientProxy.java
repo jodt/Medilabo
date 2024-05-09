f8613b4cfbb67a04cb90cb66777a450b8780d0c3
joel Dumortier <joel.dumortier@gmail.com>
2024-05-09 09:37:47 +0200

    update method

--- clientUi/src/main/java/com/medilabosolutions/clientui/proxies/PatientProxy.java
+++ clientUi/src/main/java/com/medilabosolutions/clientui/proxies/PatientProxy.java
@@ -3,8 +3,9 @@ package com.medilabosolutions.clientui.proxies;
 import com.medilabosolutions.clientui.beans.PatientBean;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @FeignClient(name = "patient-microservice", url = "localhost:9001")
@@ -12,4 +13,7 @@ public interface PatientProxy {
 
     @GetMapping("v1/api/patients/all")
     public List<PatientBean> getAllPatients ();
+
+    @GetMapping("v1/api/patients/findBySearchCriteria")
+    public List<PatientBean> getBySerachCriteria(@RequestParam String lastName, @RequestParam String firstName, @RequestParam LocalDate dateOfBirth);
 }
