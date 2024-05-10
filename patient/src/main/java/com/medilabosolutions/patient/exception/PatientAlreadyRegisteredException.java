96dbc42f0a48022d6f69e565216b6fc8ad89f2bf
joel Dumortier <joel.dumortier@gmail.com>
2024-05-10 16:24:20 +0200

    create exception

---
+++ patient/src/main/java/com/medilabosolutions/patient/exception/PatientAlreadyRegisteredException.java
@@ -0,0 +1,6 @@
+package com.medilabosolutions.patient.exception;
+
+public class PatientAlreadyRegisteredException extends Exception{
+    public PatientAlreadyRegisteredException(){super();
+    }
+}
