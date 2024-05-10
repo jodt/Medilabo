a5732cda66769b85427c033206dbd128241e2da5
joel Dumortier <joel.dumortier@gmail.com>
2024-05-10 12:18:34 +0200

    change variable type

--- patient/src/main/java/com/medilabosolutions/patient/dto/AddressDto.java
+++ patient/src/main/java/com/medilabosolutions/patient/dto/AddressDto.java
@@ -1,11 +1,12 @@
 package com.medilabosolutions.patient.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
 public class AddressDto {
 
-    private int number;
+    private Integer number;
 
     private String street;
 
