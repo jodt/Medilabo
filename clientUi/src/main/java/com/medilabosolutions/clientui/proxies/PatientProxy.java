0c086033af940027cf0c51002b0113ec74b1166f
joel Dumortier <joel.dumortier@gmail.com>
2024-05-16 12:28:03 +0200

    add method and correct spelling mistake in method name

--- clientUi/src/main/java/com/medilabosolutions/clientui/proxies/PatientProxy.java
+++ clientUi/src/main/java/com/medilabosolutions/clientui/proxies/PatientProxy.java
@@ -15,11 +15,14 @@ public interface PatientProxy {
     public List<PatientBean> getAllPatients ();
 
     @GetMapping("v1/api/patients/findBySearchCriteria")
-    public List<PatientBean> getBySerachCriteria(@RequestParam String lastName, @RequestParam String firstName, @RequestParam LocalDate dateOfBirth);
+    public List<PatientBean> getBySearchCriteria(@RequestParam String lastName, @RequestParam String firstName, @RequestParam LocalDate dateOfBirth);
 
     @PostMapping("v1/api/patients/add")
     public ResponseEntity<PatientBean> addPatient (@RequestBody PatientBean patientBean);
 
     @GetMapping("v1/api/patients/findPatientById/{id}")
     public PatientBean getPatientById(@PathVariable Integer id);
+
+    @PutMapping("v1/api/patients/update")
+    ResponseEntity<Object> updatePatient(@RequestBody PatientBean patientBean);
 }
