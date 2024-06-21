738fd305d834f30ca9f76d884f69fcc1954f3471
joel Dumortier <joel.dumortier@gmail.com>
2024-06-21 11:06:11 +0200

    add logs

--- note/src/main/java/com/medilabosolutions/note/controller/NoteController.java
+++ note/src/main/java/com/medilabosolutions/note/controller/NoteController.java
@@ -2,10 +2,12 @@ package com.medilabosolutions.note.controller;
 
 import com.medilabosolutions.note.model.Note;
 import com.medilabosolutions.note.service.NoteService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("v1/api/notes")
 public class NoteController {
@@ -18,12 +20,18 @@ public class NoteController {
 
     @GetMapping("/findByPatientId/{id}")
     public List<Note> findNotesByPatientId(@PathVariable Integer id){
-        return this.noteService.findAllNotesByPatientId(id);
+        log.info("GET /findByPatientId/{} called -> start process of finding notes for the patient with id {}", id,id);
+        List<Note> notes =  this.noteService.findAllNotesByPatientId(id);
+        log.info("{} note(s) found for the patient {}", notes.size(),id);
+        return notes;
     }
 
     @PostMapping("/add")
     public Note addPatientNote(@RequestBody Note note) {
-        return this.noteService.addNote(note);
+        log.info("POST /add called -> start the process to add a note for the patient with id {}", note.getPatientId());
+        Note noteAdded =  this.noteService.addNote(note);
+        log.info("Note added successfully");
+        return noteAdded;
     }
 
 }
--- note/src/main/java/com/medilabosolutions/note/service/NoteServiceImpl.java
+++ note/src/main/java/com/medilabosolutions/note/service/NoteServiceImpl.java
@@ -2,10 +2,12 @@ package com.medilabosolutions.note.service;
 
 import com.medilabosolutions.note.model.Note;
 import com.medilabosolutions.note.repository.NoteRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class NoteServiceImpl implements NoteService {
 
@@ -17,11 +19,13 @@ public class NoteServiceImpl implements NoteService {
 
     @Override
     public List<Note> findAllNotesByPatientId(Integer id) {
+        log.info("Try to find notes for the patient with id {}", id);
         return this.noteRepository.findNoteByPatientId(id);
     }
 
     @Override
     public Note addNote(Note note) {
-       return this.noteRepository.insert(note);
+        log.info("Try to add note for the patient with id {}", note.getPatientId());
+        return this.noteRepository.insert(note);
     }
 }
