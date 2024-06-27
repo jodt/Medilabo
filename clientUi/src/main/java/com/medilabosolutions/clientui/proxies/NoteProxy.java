eb37d24e81bc01b41ab7ba2b57e3f859d06e1b45
joel Dumortier <joel.dumortier@gmail.com>
2024-06-27 09:31:04 +0200

    update return type

--- clientUi/src/main/java/com/medilabosolutions/clientui/proxies/NoteProxy.java
+++ clientUi/src/main/java/com/medilabosolutions/clientui/proxies/NoteProxy.java
@@ -2,6 +2,7 @@ package com.medilabosolutions.clientui.proxies;
 
 import com.medilabosolutions.clientui.beans.NoteBean;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,6 @@ public interface NoteProxy {
     public List<NoteBean> findNotesByPatientId(@PathVariable Integer id);
 
     @GetMapping("v1/api/notes/add")
-    public NoteBean addPatientNote(@RequestBody NoteBean note);
+    public ResponseEntity<NoteBean> addPatientNote(@RequestBody NoteBean note);
 
 }
