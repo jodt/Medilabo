de2c55da6664228b8b73162805dbddbdfda1aaf5
joel Dumortier <joel.dumortier@gmail.com>
2024-06-27 09:28:23 +0200

    update post method to return ResponseEntity with created status

--- note/src/main/java/com/medilabosolutions/note/controller/NoteController.java
+++ note/src/main/java/com/medilabosolutions/note/controller/NoteController.java
@@ -4,6 +4,8 @@ import com.medilabosolutions.note.dto.NoteDto;
 import com.medilabosolutions.note.model.Note;
 import com.medilabosolutions.note.service.NoteService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,11 +30,11 @@ public class NoteController {
     }
 
     @PostMapping("/add")
-    public Note addPatientNote(@RequestBody NoteDto note) {
+    public ResponseEntity<Note> addPatientNote(@RequestBody NoteDto note) {
         log.info("POST /add called -> start the process to add a note for the patient with id {}", note.getPatientId());
         Note noteAdded =  this.noteService.addNote(note);
         log.info("Note added successfully");
-        return noteAdded;
+        return new ResponseEntity<>(noteAdded, HttpStatus.CREATED);
     }
 
 }
