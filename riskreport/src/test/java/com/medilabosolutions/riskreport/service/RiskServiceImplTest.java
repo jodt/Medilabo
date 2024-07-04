f8cb9aacba329513f5112c7b14963052e8aaeff3
joel Dumortier <joel.dumortier@gmail.com>
2024-07-04 16:31:26 +0200

    create riskService unit tests

---
+++ riskreport/src/test/java/com/medilabosolutions/riskreport/service/RiskServiceImplTest.java
@@ -0,0 +1,310 @@
+package com.medilabosolutions.riskreport.service;
+
+import com.medilabosolutions.riskreport.beans.GenderEnum;
+import com.medilabosolutions.riskreport.beans.NoteBean;
+import com.medilabosolutions.riskreport.beans.PatientBean;
+import com.medilabosolutions.riskreport.exceptions.ResourceNotFoundException;
+import com.medilabosolutions.riskreport.proxies.NoteProxy;
+import com.medilabosolutions.riskreport.proxies.PatientProxy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class RiskServiceImplTest {
+
+    @Mock
+    PatientProxy patientProxy;
+
+    @Mock
+    NoteProxy noteProxy;
+
+    @InjectMocks
+    RiskServiceImpl riskService;
+
+    @Test
+    @DisplayName("Should calculate the risk for a man under thirty -> None")
+    void ShouldCalculatePatientRiskForManUnderThirtyWithRiskNone() throws ResourceNotFoundException {
+
+        NoteBean note = NoteBean.builder()
+                .patientId(1)
+                .content("Le patient déclare qu'il 'se sent très bien' Poids égal ou inférieur au poids recommandé")
+                .build();
+
+        PatientBean patient = PatientBean.builder()
+                .id(1)
+                .dateOfBirth(LocalDate.of(1999, 8, 22))
+                .gender(GenderEnum.M)
+                .build();
+
+        when(this.patientProxy.getPatientById(1)).thenReturn(patient);
+        when(this.noteProxy.findNotesByPatientId(1)).thenReturn(List.of(note));
+
+        String result = riskService.calculPatientRisk(1);
+
+        assertNotNull(result);
+        assertEquals("None", result);
+
+        verify(this.patientProxy).getPatientById(1);
+        verify(this.noteProxy).findNotesByPatientId(1);
+    }
+
+    @Test
+    @DisplayName("Should calculate the risk for a man under thirty -> InDanger")
+    void ShouldCalculatePatientRiskForManUnderThirtyWithRiskInDanger() throws ResourceNotFoundException {
+
+        NoteBean note = NoteBean.builder()
+                .patientId(1)
+                .content("Le patient, fumeur, déclare qu'il a des vertiges. Poids supérieur au poids recommandé")
+                .build();
+
+
+        PatientBean patient = PatientBean.builder()
+                .id(1)
+                .dateOfBirth(LocalDate.of(1999, 8, 22))
+                .gender(GenderEnum.M)
+                .build();
+
+        when(this.patientProxy.getPatientById(1)).thenReturn(patient);
+        when(this.noteProxy.findNotesByPatientId(1)).thenReturn(List.of(note));
+
+        String result = riskService.calculPatientRisk(1);
+
+        assertNotNull(result);
+        assertEquals("InDanger", result);
+
+        verify(this.patientProxy).getPatientById(1);
+        verify(this.noteProxy).findNotesByPatientId(1);
+    }
+
+    @Test
+    @DisplayName("Should calculate the risk for a man under thirty -> EarlyOnset")
+    void ShouldCalculatePatientRiskForManUnderThirtyWithRiskEarlyOnSet() throws ResourceNotFoundException {
+
+        NoteBean note = NoteBean.builder()
+                .patientId(1)
+                .content("Le patient, fumeur, déclare qu'il a des vertiges. Poids supérieur au poids recommandé. Cholestérol anormal")
+                .build();
+
+
+        PatientBean patient = PatientBean.builder()
+                .id(1)
+                .dateOfBirth(LocalDate.of(1999, 8, 22))
+                .gender(GenderEnum.M)
+                .build();
+
+        when(this.patientProxy.getPatientById(1)).thenReturn(patient);
+        when(this.noteProxy.findNotesByPatientId(1)).thenReturn(List.of(note));
+
+        String result = riskService.calculPatientRisk(1);
+
+        assertNotNull(result);
+        assertEquals("EarlyOnset", result);
+
+        verify(this.patientProxy).getPatientById(1);
+        verify(this.noteProxy).findNotesByPatientId(1);
+    }
+
+    @Test
+    @DisplayName("Should calculate the risk for a woman under thirty -> None")
+    void ShouldCalculatePatientRiskForWomanUnderThirtyWithRiskNone() throws ResourceNotFoundException {
+
+        NoteBean note = NoteBean.builder()
+                .patientId(1)
+                .content("La patiente déclare qu'elle 'se sent très bien' Poids égal ou inférieur au poids recommandé")
+                .build();
+
+        PatientBean patient = PatientBean.builder()
+                .id(1)
+                .dateOfBirth(LocalDate.of(1999, 8, 22))
+                .gender(GenderEnum.F)
+                .build();
+
+        when(this.patientProxy.getPatientById(1)).thenReturn(patient);
+        when(this.noteProxy.findNotesByPatientId(1)).thenReturn(List.of(note));
+
+        String result = riskService.calculPatientRisk(1);
+
+        assertNotNull(result);
+        assertEquals("None", result);
+
+        verify(this.patientProxy).getPatientById(1);
+        verify(this.noteProxy).findNotesByPatientId(1);
+    }
+
+    @Test
+    @DisplayName("Should calculate the risk for a woman under thirty -> InDanger")
+    void ShouldCalculatePatientRiskForWomanUnderThirtyWithRiskInDanger() throws ResourceNotFoundException {
+
+        NoteBean note = NoteBean.builder()
+                .patientId(1)
+                .content("La patiente, fumeuse, déclare qu'elle a des vertiges et réaction allergique aux medicaments. Poids supérieur au poids recommandé")
+                .build();
+
+
+        PatientBean patient = PatientBean.builder()
+                .id(1)
+                .dateOfBirth(LocalDate.of(1999, 8, 22))
+                .gender(GenderEnum.F)
+                .build();
+
+        when(this.patientProxy.getPatientById(1)).thenReturn(patient);
+        when(this.noteProxy.findNotesByPatientId(1)).thenReturn(List.of(note));
+
+        String result = riskService.calculPatientRisk(1);
+
+        assertNotNull(result);
+        assertEquals("InDanger", result);
+
+        verify(this.patientProxy).getPatientById(1);
+        verify(this.noteProxy).findNotesByPatientId(1);
+    }
+
+    @Test
+    @DisplayName("Should calculate the risk for a woman under thirty -> EarlyOnset")
+    void ShouldCalculatePatientRiskForWomanUnderThirtyWithRiskEarlyOnset() throws ResourceNotFoundException {
+
+        NoteBean note = NoteBean.builder()
+                .patientId(1)
+                .content("La patiente, fumeuse, déclare qu'elle a des vertiges et réaction allergique aux medicaments. Poids supérieur au poids recommandé. Cholestérol anormal. Rechute")
+                .build();
+
+
+        PatientBean patient = PatientBean.builder()
+                .id(1)
+                .dateOfBirth(LocalDate.of(1999, 8, 22))
+                .gender(GenderEnum.F)
+                .build();
+
+        when(this.patientProxy.getPatientById(1)).thenReturn(patient);
+        when(this.noteProxy.findNotesByPatientId(1)).thenReturn(List.of(note));
+
+        String result = riskService.calculPatientRisk(1);
+
+        assertNotNull(result);
+        assertEquals("EarlyOnset", result);
+
+        verify(this.patientProxy).getPatientById(1);
+        verify(this.noteProxy).findNotesByPatientId(1);
+    }
+
+    @Test
+    @DisplayName("Should calculate the risk for a man over thirty -> None")
+    void ShouldCalculatePatientRiskForManOverThirtyWithRiskNone() throws ResourceNotFoundException {
+
+        NoteBean note = NoteBean.builder()
+                .patientId(1)
+                .content("Le patient déclare qu'il 'se sent très bien' Poids égal ou inférieur au poids recommandé")
+                .build();
+
+        PatientBean patient = PatientBean.builder()
+                .id(1)
+                .dateOfBirth(LocalDate.of(1950, 8, 22))
+                .gender(GenderEnum.M)
+                .build();
+
+        when(this.patientProxy.getPatientById(1)).thenReturn(patient);
+        when(this.noteProxy.findNotesByPatientId(1)).thenReturn(List.of(note));
+
+        String result = riskService.calculPatientRisk(1);
+
+        assertNotNull(result);
+        assertEquals("None", result);
+
+        verify(this.patientProxy).getPatientById(1);
+        verify(this.noteProxy).findNotesByPatientId(1);
+    }
+
+    @Test
+    @DisplayName("Should calculate the risk for a man over thirty -> Borderline")
+    void ShouldCalculatePatientRiskForManOverThirtyWithRiskBorderline() throws ResourceNotFoundException {
+
+        NoteBean note = NoteBean.builder()
+                .patientId(1)
+                .content("Poids supérieur au poids recommandé. Cholestérol anormal")
+                .build();
+
+        PatientBean patient = PatientBean.builder()
+                .id(1)
+                .dateOfBirth(LocalDate.of(1950, 8, 22))
+                .gender(GenderEnum.M)
+                .build();
+
+        when(this.patientProxy.getPatientById(1)).thenReturn(patient);
+        when(this.noteProxy.findNotesByPatientId(1)).thenReturn(List.of(note));
+
+        String result = riskService.calculPatientRisk(1);
+
+        assertNotNull(result);
+        assertEquals("Borderline", result);
+
+        verify(this.patientProxy).getPatientById(1);
+        verify(this.noteProxy).findNotesByPatientId(1);
+    }
+
+    @Test
+    @DisplayName("Should calculate the risk for a man over thirty -> InDanger")
+    void ShouldCalculatePatientRiskForManOverThirtyWithRiskInDanger() throws ResourceNotFoundException {
+
+        NoteBean note = NoteBean.builder()
+                .patientId(1)
+                .content("Poids supérieur au poids recommandé. Fumeur. Cholestérol anormal. Vertiges. Rechute.")
+                .build();
+
+        PatientBean patient = PatientBean.builder()
+                .id(1)
+                .dateOfBirth(LocalDate.of(1950, 8, 22))
+                .gender(GenderEnum.M)
+                .build();
+
+        when(this.patientProxy.getPatientById(1)).thenReturn(patient);
+        when(this.noteProxy.findNotesByPatientId(1)).thenReturn(List.of(note));
+
+        String result = riskService.calculPatientRisk(1);
+
+        assertNotNull(result);
+        assertEquals("InDanger", result);
+
+        verify(this.patientProxy).getPatientById(1);
+        verify(this.noteProxy).findNotesByPatientId(1);
+    }
+
+    @Test
+    @DisplayName("Should calculate the risk for a man over thirty -> EarlyOnset")
+    void ShouldCalculatePatientRiskForManOverThirtyWithRiskEarlyOnset() throws ResourceNotFoundException {
+
+        NoteBean note = NoteBean.builder()
+                .patientId(1)
+                .content("Poids supérieur au poids recommandé. Fumeur. Cholestérol anormal. Vertiges. Rechute. Réaction allergique. Anticorps bas")
+                .build();
+
+        PatientBean patient = PatientBean.builder()
+                .id(1)
+                .dateOfBirth(LocalDate.of(1950, 8, 22))
+                .gender(GenderEnum.M)
+                .build();
+
+        when(this.patientProxy.getPatientById(1)).thenReturn(patient);
+        when(this.noteProxy.findNotesByPatientId(1)).thenReturn(List.of(note));
+
+        String result = riskService.calculPatientRisk(1);
+
+        assertNotNull(result);
+        assertEquals("EarlyOnset", result);
+
+        verify(this.patientProxy).getPatientById(1);
+        verify(this.noteProxy).findNotesByPatientId(1);
+    }
+
+}
