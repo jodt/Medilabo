6df994a7a4c0ff1589e5b92b5f2bcbb2b54108b4
joel Dumortier <joel.dumortier@gmail.com>
2024-09-13 12:17:23 +0200

    add validation constraints and allow errors to be displayed for the phone number field

--- clientUi/src/main/java/com/medilabosolutions/clientui/beans/PatientBean.java
+++ clientUi/src/main/java/com/medilabosolutions/clientui/beans/PatientBean.java
@@ -5,6 +5,7 @@ import com.medilabosolutions.clientui.validation.ValidDateOfBirth;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,10 +23,12 @@ public class PatientBean {
 
     @NotNull
     @NotBlank(message = "LastName is mandatory")
+    @Size(max = 150, message = "Size is limited to 150 characters")
     private String lastName;
 
     @NotNull
     @NotBlank(message = "FirstName is mandatory")
+    @Size(max = 100, message = "Size is limited to 100 characters")
     private String firstName;
 
     @NotNull(message = "Date Of Birth is mandatory")
@@ -38,6 +41,7 @@ public class PatientBean {
     @Valid
     private AddressBean address;
 
+    @Size(max = 30, message = "Size is limited to 30 characters")
     private String phoneNumber;
 
 
